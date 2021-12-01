be780d32bb969296d58640cbb3ed0f33520f1a3c
audwodbs492 <audwodbs492@gmail.com>
2021-12-01 11:43:53 +0900

    2021-12-01 그래프 색상 변경

--- Partner/partnerclient/components/charts/DateByProfit.tsx
+++ Partner/partnerclient/components/charts/DateByProfit.tsx
@@ -25,44 +25,28 @@ const DateByProfit = ({ data }: Prop) => {
     series: [
       { name: "일자", data: data && data.map((item) => item.totalPayment) },
     ],
-    // series: [
-    //   {
-    //     name: "일자",
-    //     data: [
-    //       153400, 124000, 323040, 180000, 23000, 140000, 50123, 45000, 300000,
-    //       210000,
-    //     ],
-    //   },
-    // ],
     options: {
       chart: {
         toolbar: {
           show: false,
         },
       },
-      // x축의 라벨
       xaxis: {
-        // categories: [
-        //   "11-01",
-        //   "11-02",
-        //   "11-03",
-        //   "11-04",
-        //   "11-05",
-        //   "11-06",
-        //   "11-07",
-        //   "11-08",
-        //   "11-09",
-        //   "11-10",
-        // ],
         categories: data && data.map((item) => item.orderDate),
       },
-      // y축의 형식을 지정
       yaxis: {
         labels: {
           formatter: function (val: number) {
             return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
           },
         },
+      },
+      fill: {
+        colors: [
+          () => {
+            return "rgb(191, 103, 27)";
+          },
+        ],
       },
     },
   };
